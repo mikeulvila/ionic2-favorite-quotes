707e4d371176c048214c6de7ff3c7103720542f8
Mike Ulvila <mike.ulvila@premisehealth.com>
2017-02-09 20:34:55 -0600

    setup tabs navigation

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import {FavoritesPage} from "../favorites/favorites";
+import {LibraryPage} from "../library/library";
+
+@Component({
+  selector: 'page-tabs',
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+  favoritesPage = FavoritesPage;
+  libraryPage = LibraryPage;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+
+}
