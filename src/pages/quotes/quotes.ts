dffde19f6ba52cef0e9232224f0cb1502d402dbf
Mike Ulvila <mike.ulvila@premisehealth.com>
2017-02-09 21:48:01 -0600

    add alert to app

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {NavController, NavParams, AlertController} from 'ionic-angular';
 import { Quote } from '../../data/quote.interface'
 @Component({
   selector: 'page-quotes',
@@ -8,7 +8,9 @@ import { Quote } from '../../data/quote.interface'
 export class QuotesPage implements OnInit {
   quoteGroup: {category: string, quotes: Quote[], icon: string};
 
-  constructor(public navCtrl: NavController, private navParams: NavParams) {}
+  constructor(public navCtrl: NavController,
+              private navParams: NavParams,
+              private alertCtrl: AlertController) {}
 
   ngOnInit() {
     this.quoteGroup = this.navParams.data;
@@ -17,4 +19,28 @@ export class QuotesPage implements OnInit {
   //   this.quoteGroup = this.navParams.data;
   //  Add Elvis operator (?) in template to use this approach: quoteGroup?.category
   // }
+
+  onAddToFavorite(selectedQuote: Quote) {
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {
+          text: 'Yes',
+          handler: () => {
+            console.log('Yes');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel');
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
 }
