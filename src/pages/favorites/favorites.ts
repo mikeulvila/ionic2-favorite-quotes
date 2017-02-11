a47afc4b5e778996a6694f6aae317093d0967ee4
Mike Ulvila <mike.ulvila@premisehealth.com>
2017-02-11 09:55:56 -0600

    implement background color toggle

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -3,6 +3,7 @@ import {ModalController} from 'ionic-angular';
 import {QuotesService} from "../../services/quotes.service";
 import {Quote} from "../../data/quote.interface";
 import {QuotePage} from "../quote/quote";
+import {SettingsService} from "../../services/settings.service";
 
 @Component({
   selector: 'page-favorites',
@@ -11,7 +12,8 @@ import {QuotePage} from "../quote/quote";
 export class FavoritesPage {
   quotes: Quote[];
   constructor(private quotesService: QuotesService,
-              private modalCtrl: ModalController) {}
+              private modalCtrl: ModalController,
+              private settingsService: SettingsService) {}
 
   ionViewWillEnter() {
     this.quotes = this.quotesService.getFavoriteQuotes();
@@ -33,6 +35,14 @@ export class FavoritesPage {
       return quoteEl.id === quote.id;
     });
     this.quotes.splice(position, 1);
+  }
+
+  getBackground() {
+    return this.settingsService.isAltBackground() ? 'altQuoteBackground' : 'quoteBackground';
+  }
+
+  isAltBackground() {
+    return this.settingsService.isAltBackground();
   }
 
 }
