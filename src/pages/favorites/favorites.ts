c6e98b03d7e8e8e27eab4221333077f9a856dbc1
Mike Ulvila <mike.ulvila@premisehealth.com>
2017-02-11 08:35:54 -0600

    delete quote using ion-sliding

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -24,13 +24,17 @@ export class FavoritesPage {
     modal.present();
     modal.onDidDismiss((remove: boolean) => {
       if (remove) {
-        this.quotesService.removeQuoteFromFavorites(quote);
-        const position = this.quotes.findIndex((quoteEl) => {
-          return quoteEl.id === quote.id;
-        });
-        this.quotes.splice(position, 1);
+        this.onRemoveFromFavorites(quote);
       }
     });
+  }
+
+  onRemoveFromFavorites(quote: Quote) {
+    this.quotesService.removeQuoteFromFavorites(quote);
+    const position = this.quotes.findIndex((quoteEl) => {
+      return quoteEl.id === quote.id;
+    });
+    this.quotes.splice(position, 1);
   }
 
 
