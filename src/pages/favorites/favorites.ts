4086827c148d98cacd627132d5ca29dd3c555957
Mike Ulvila <mike.ulvila@premisehealth.com>
2017-02-10 19:02:42 -0600

    set up modal to display quote

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {NavController, NavParams, ModalController} from 'ionic-angular';
 import {QuotesService} from "../../services/quotes.service";
 import {Quote} from "../../data/quote.interface";
+import {QuotePage} from "../quote/quote";
 
 @Component({
   selector: 'page-favorites',
@@ -11,10 +12,16 @@ export class FavoritesPage {
   quotes: Quote[];
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              private quotesService: QuotesService) {}
+              private quotesService: QuotesService,
+              private modalCtrl: ModalController) {}
 
   ionViewWillEnter() {
     this.quotes = this.quotesService.getFavoriteQuotes();
+  }
+
+  onViewQuote(quote: Quote) {
+    const modal = this.modalCtrl.create(QuotePage);
+    modal.present();
   }
 
 
